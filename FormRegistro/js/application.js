497abaabfdacbf0ddc443928fe69e1557add1bc3
Seba <sebastianpanetta@gmail.com>
2020-03-29 18:14:58 -0300

    Agregado de Steps 3 y 4
    - Agregado de steps Datos de Contacto y Datos Laborales

--- FormRegistro/js/application.js
+++ FormRegistro/js/application.js
@@ -1,138 +1,74 @@
 $(document).ready(function () {
 
-	$('input').on('blur', function () {
-		$('#helpInfo').html("Información adicional");
-	});
-
 	$('input').on('focus', function () {
 		if ($(this).data('info'))
 			$('#helpInfo').html($(this).data('info'));
 	});
 
-	$('#txtphone').mask('(00) 00000-0000');
+	$('#txtTelefono').mask('(00) 00000-0000');
 
 });
 
-function checkNacionalidad() {
-	var object = event.target;
-
-	console.log(event.target.value);
-
-	$.get("json/buenos-aires.json", function (data) {
-		console.log("Martin");
-	});
-
-	//console.log($(event.target).children(":first").data('ort'));
-
-	if (event.target.value == "ciudad") {
-		$("#cmbLocalidad").parent().show('slow');
-	} else {
-		$("#cmbLocalidad").parent().hide('slow');
-		//document.getElementById('cmbLocalidad').parentNode.setAttribute('style', 'display:none');
-	}
-
-}
-
-function validarForm(e) {
-	var first = false;
-	var percent = 0;
-	var error = false;
-
-	try {
-		if (document.getElementById('txtNombre').value == "") {
-			error = true;
-			document.getElementById('txtNombre').setAttribute('class', 'form-control is-invalid');
-		} else {
-			percent += 10;
-			//Con JQuery
-			$('#txtNombre').removeClass('is-invalid').addClass('is-valid');
-		}
-
-		if (document.getElementById('txtApellido').value == "") {
-			$('#txtApellido').addClass('is-invalid');
-			error = true;
-		} else {
-			percent += 10;
-			$('#txtApellido').removeClass('is-invalid').addClass('is-valid');
-		}
-
-		if (document.getElementById('txtDni').value == ""
-			|| !Number(document.getElementById('txtDni').value)) {
-			$('#txtDni').addClass('is-invalid');
-			error = true;
-		} else {
-			percent += 10;
-			$('#txtDni').removeClass('is-invalid').addClass('is-valid');
-		}
-
-		if ($('#cmbProvincia').val() == "") {
-			$('#cmbProvincia').addClass('is-invalid');
-			error = true;
-		} else {
-			percent += 10;
-			$('#cmbProvincia').removeClass('is-invalid').addClass('is-valid');
-		}
-
-		if (document.getElementById('txtEmail').value.indexOf("@") < 0
-			|| document.getElementById('txtEmail').value.indexOf(".") < 0
-			|| document.getElementById('txtEmail').value.length < 4) {
-			$('#txtEmail').addClass('is-invalid');
-			error = true;
-		} else {
-			percent += 20;
-			$('#txtEmail').removeClass('is-invalid').addClass('is-valid');
-		}
-
-		if ($('#txtphone').val() == "") {
-			error = true;
-			$('#txtphone').addClass('is-invalid');
-		} else {
-			percent += 20;
-			$('#txtphone').removeClass('is-invalid').addClass('is-valid');
-		}
-
-		if (!$("#chkTerms").is(':checked')) {
-
-			$('#chkTerms').parents(".form-check:first").addClass('wobble animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-				$(this).removeClass('wobble animated');
-			});
-		}
-
-		$(".progress-bar").css('width', percent + '%');
-
-		return false;
-
-	} catch (e) {
-		console.log(e);
-	}
-
-	return error ? false : true;
-}
-
-function continuar(step) {
+function changeStep(step) {
 	switch (step) {
 		case 1:
+			renderIndex();
+			break;
+		case 2:
 			renderDomicilio();
+			break;
+		case 3:
+			renderContacto();
+			break;
+		case 4:
+			renderDatosLaborales();
 			break;
 	}
 }
 
-// renderizar el step domicilio y ocultar el step anterior
-function renderDomicilio() {
-	let thisStep = $("#step1");
+function renderIndex() {
+	let thisStep = $("#step2");
 	thisStep.prop("hidden", true);
+	let nextStep = $("#step1");
+	nextStep.prop("hidden", false);
+}
+
+// renderizar domicilio y ocultar el step anterior
+function renderDomicilio() {
+	let thisStep1 = $("#step1");
+	thisStep1.prop("hidden", true);
+	let thisStep3 = $("#step3");
+	thisStep3.prop("hidden", true);
 	let nextStep = $("#step2");
+	nextStep.prop("hidden", false);
+}
+
+function renderContacto() {
+	let thisStep2 = $("#step2");
+	thisStep2.prop("hidden", true);
+	let thisStep4 = $("#step4");
+	thisStep4.prop("hidden", true);
+	let nextStep = $("#step3");
+	nextStep.prop("hidden", false);
+}
+
+function renderDatosLaborales() {
+	let thisStep3 = $("#step3");
+	thisStep3.prop("hidden", true);
+	let thisStep5 = $("#step5");
+	thisStep5.prop("hidden", true);
+	let nextStep = $("#step4");
 	nextStep.prop("hidden", false);
 }
 
 // al cambiar el combo box provincia, renderizar localidades
 $("#cmbProvincia").change(function () {
 	if ($(this).val() == "") {
-		$('#localidad').hide();
+		$('#localidad').prop("hidden",true);
 		$('#cmbLocalidad').removeAttr('required');
 		$('#cmbLocalidad').removeAttr('data-error');
 	} else {
-		$('#localidad').show();
+		$('#localidad').prop("hidden",false);
 		$('#cmbLocalidad').attr('required', '');
 		$('#cmbLocalidad').attr('data-error', 'This field is required.');
 
